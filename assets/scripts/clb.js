e42bbca8cbfe1599dbbadfe35c62ca663eea71de
Ceo-Sammarco <noah.kd@gmail.com>
2024-11-18 23:44:10 -0300

    @Ceo-Sammarco - seg 18 nov 24

--- assets/scripts/clb.js
+++ assets/scripts/clb.js
@@ -121,4 +121,160 @@ window.addEventListener( "load", ev => {
       );
    }
 
+
+
+   /**
+    * doc-title 
+    * */
+   $( 'doc-title', 1 ).forEach( ( title, i ) => {
+      let 
+         data = title.getAttribute( 'subtitle' ) ? `
+            <t style='font-weight: 700; color: #00559c'>
+               ${ title.getAttribute( 'subtitle' ) }
+            </t>
+         ` : null
+      ;
+
+      return( title.innerHTML = `
+         ${ data && data }
+         <t style='font-weight: 700; color: #003b6b\'>
+            ${ title.innerHTML }
+         </t>
+      ` );
+      
+   } );
+
+   /**
+    * article 
+    * */
+   $( 'article', 1 ).forEach( ( article, i ) => {
+      let 
+         children = article.innerHTML 
+      ;
+      if( article.getAttribute( "label" ) ) {
+         article.innerHTML = `
+            <ui>
+               <header>
+                  <ttl>
+                     ${ article.getAttribute( 'label' ) }
+                  </ttl>
+               </header>
+               <content>
+                  ${ children }
+               </content>
+            </ui>
+         `;
+      }
+   } );
+
+   /**
+    * section 
+    * */
+   $( 'section', 1 ).forEach( ( section, i ) => {
+      let 
+         children = section.innerHTML 
+      ;
+      if( section.getAttribute( "label" ) ) {
+         section.innerHTML = `
+            <ui>
+               <header>
+                  <t6>
+                     ${ section.getAttribute( 'label' ) }
+                  </t6>
+               </header>
+               <content>
+                  ${ children }
+               </content>
+            </ui>
+         `;
+      }
+   } );
+
+
+   /**
+    * cliente 
+    * */
+   $( 'cliente', 1 ).forEach( ( cliente, i ) => {
+      let 
+         children = cliente.innerHTML 
+         ,
+         nome = `
+            <t>
+               <b>Nome </b> ${ cliente.getAttribute( 'nome' ) }
+            </t>` || null
+         ,
+         endereço = `
+            <t>
+               <b>Endereço </b>
+               ${ cliente.getAttribute( 'endereço' )  }
+            </t>` || null
+      ;
+      cliente.innerHTML = `
+         <ui>
+            <header>
+               <ui>
+                  Cliente 
+               </ui>
+            </header>
+            <content>
+               <card>
+                  <ui>
+                     ${ nome && nome }
+                     ${ endereço && endereço }
+                     ${ children && children }
+                  </ui>
+               </card>
+            </content>
+         </ui>
+      `;
+   } );
+
+
+   /**
+    * assinaturas 
+    * */
+   $( 'signatures', 1 ).forEach( ( signatures, i ) => {
+      let 
+         children = signatures.innerHTML
+      ;
+      signatures.innerHTML = `
+         <!-- <section>
+            <p pdh>
+               Declaro estar ciente e de acordo com o presente orçamento:
+            </p>
+         </section> -->
+         <signature section>
+            <content>
+
+               <!-- <sig-name>Assinatura do Responsável Técnico</sig-name> -->
+               <sig-name>Rafael - Elétrica&Art</sig-name>
+               
+               <!-- Elétrica&Art  -->
+               <!--Rafael - Elétrica&Art-->
+            </content>
+         </signature>
+         <signature section>
+            <content>
+               <sig-name>Assinatura do Cliente</sig-name> 
+
+            </content>
+         </signature>
+         <!-- <content>
+            <p style="text-align: center;">TESTEMUNHAS</p>
+         </content>
+         <row>
+            <signature section>
+               <content>
+                  <sig-name>CPF</sig-name>
+               </content>
+            </signature>
+            <signature section>
+               <content>
+                  <sig-name>CPF</sig-name>
+               </content>
+            </signature>
+         </row> -->
+      `;
+   } );
+
 } );
