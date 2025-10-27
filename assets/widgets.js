06423fcf61846216ce725b6bd4a7a466758bc953
Ceo-Sammarco <noah.kd@gmail.com>
2025-10-27 14:00:05 -0300

    @Ceo-Sammarco - 27/10/2025

--- assets/widgets.js
+++ assets/widgets.js
@@ -4,74 +4,49 @@
 /* == [ properties ]
 == == == == == == == == == */
 const 
-   _ = ( ...v ) => console.log( ...v )
-   ,
-   table = v => console.table( v )
-   ,
-   /*$ = ( element, isAllNodes ) => {
-      if( !isAllNodes ) {
-         return document.querySelector( element );
-      } else {
-         return document.querySelectorAll( element );
-      }
-   }
-   ,*/
-   toBrl = v => v.toLocaleString( "pt-br", { "style": "currency", "currency": "BRL" } )
-   ,
-   brlToInt = v => {
-      return(
-         parseInt( v.replace( "R$", "" ).replaceAll( ".", "" ).replace( ",00", "" ) )
-      );
-   } 
-   ,
-   brlToFloat = v => {
-      return(
-         parseFloat( v.replace( "R$", "" ).replaceAll( ".", "" ).replace( ",", "." ) )
-      );
-   } 
-   ,
-   fix = v => v.toFixed( 2 )
-   ,
-   add0 = v => {
-      if( v < 10 ) {
-         return( `0${ v }` );
-      } else {
-         return( v );
-      }
-   }
+  _ = ( ...v ) => console.log( ...v )
+  ,
+  table = v => console.table( v )
+  ,
+  toBrl = v => v.toLocaleString( "pt-br", { "style": "currency", "currency": "BRL" } )
+  ,
+  brlToInt = v => {
+    return(
+      parseInt( v.replace( "R$", "" ).replaceAll( ".", "" ).replace( ",00", "" ) )
+    );
+  } 
+  ,
+  brlToFloat = v => {
+    return(
+      parseFloat( v.replace( "R$", "" ).replaceAll( ".", "" ).replace( ",", "." ) )
+    );
+  } 
+  ,
+  fix = v => v.toFixed( 2 )
+  ,
+  add0 = v => {
+    if( v < 10 ) {
+      return( `0${ v }` );
+    } else {
+      return( v );
+    }
+  }
 ;
 
 function FormatNumber( link ) {
-   let data = link;
-   data = data.replaceAll( /\(/gi, "" );
-   data = data.replaceAll( /\)/gi, "" );
-   data = data.replaceAll( /\-/gi, "" );
-   data = data.replaceAll( /\ /gi, "" );
-   return data;
+  let data = link;
+  data = data.replaceAll( /\(/gi, "" );
+  data = data.replaceAll( /\)/gi, "" );
+  data = data.replaceAll( /\-/gi, "" );
+  data = data.replaceAll( /\ /gi, "" );
+  return data;
 }
 
-/*HTMLElement.prototype.$ = function( v ) {
-   return( this.querySelectorAll( v ) );
-};
-
-HTMLElement.prototype.attribute = function( v ) {
-   return( 
-      this.getAttribute( v )
-   );
-};*/
-
 String.prototype.data = function( v ) {
-   return(
-      JSON.parse( this )
-   );
+  return(
+    JSON.parse( this )
+  );
 };
-
-/*HTMLElement.prototype.attribute = function( v ) {
-   return( 
-      this.getAttribute( v )
-   );
-};*/
-// _( "HTMLElement: ", submit_calcular.$a( "type" ) );
 
 /* -------------------------------- */
 
@@ -81,19 +56,19 @@ String.prototype.data = function( v ) {
 == == == == == == == == == */
 window.addEventListener( "load", ev => {
 	
-   /* == [ navlink ] 
-   == == == == == == == == == */
-   $$( "navlink" ).forEach( nl => {  
-      _( "oi" );
-      nl.outerHTML = `
-         <p>
-            <a href="${ nl.getAttribute( "to" ) }"
-               target="_blank" >
-               ${ nl.innerText }
-            </a>
-         </p>
-      `;
-   } );
+  /* == [ navlink ] 
+  == == == == == == == == == */
+  $$( "navlink" ).forEach( nl => {  
+    _( "oi" );
+    nl.outerHTML = `
+      <p>
+        <a href="${ nl.getAttribute( "to" ) }"
+          target="_blank" >
+            ${ nl.innerText }
+        </a>
+      </p>
+    `;
+  } );
 
    $$( "[link]" ).forEach( link => {
       link.addEventListener( "click", () => {
